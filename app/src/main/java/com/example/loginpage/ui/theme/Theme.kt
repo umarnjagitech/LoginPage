313bd7d01f1cabb266ec80c9a940e9033ca01536
umarnjagitech <omarnjagi@gmail.com>
2025-04-03 17:43:12 +0300

    Project Build Upgrades: Gradle

--- app/src/main/java/com/example/loginpage/ui/theme/Theme.kt
+++ app/src/main/java/com/example/loginpage/ui/theme/Theme.kt
@@ -10,6 +10,10 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.SideEffect
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Pink60,
@@ -50,6 +54,14 @@ fun LoginPageTheme(
 
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = false
+        }
     }
 
     MaterialTheme(
